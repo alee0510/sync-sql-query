da965a7ec7fa0d88fc606ba261ed8d299d70655b
alee0510 <ali.muksin0510@gmail.com>
2020-08-16 02:19:24 +0700

    simplified query function and add for pool connection

--- index.js
+++ index.js
@@ -1,48 +1,71 @@
-class Connection {
-    constructor(database) {
-        this.database = database
+class SingleConnection {
+    constructor(db) {
+        this._db = db
     }
-    // create promisfy our query
-    databaseQuery = (query, escape = []) => {
-        return new Promise ((resolve, reject) => {
-            this.database.query(query, escape, (err, result) => {
-                if (err) {
-                    reject(err.sqlMessage)
-                } else {
-                    resolve(result)
+
+    // create custom promisfy from query function
+    dbQuery = (query, escape = []) => {
+        return new Promise((resolve, reject) => {
+            this._db.query(query, escape, (err, result) => {
+                // check err
+                err ? reject({code : 500, msg : err.sqlMessage}) : resolve(result)
+            })
+        })
+    }
+
+    // error handling for begin transaction 
+    dbQueryTransactionWithErrorHandle = async (res, callback) => {
+        await this._db.beginTransaction( err => {
+            try {
+                // check error
+                if (err) throw ({ code : 500, msg : err })
+
+                // our query function
+                await callback()
+                
+                // commit if theres no error
+                await this.dbQuery.commit()
+            } catch (err) {
+                await this._db.rollback()
+                res.status(err.code).send(err.msg)
+            }
+        })
+    }
+}
+
+class PoolConnection {
+    dbPoolQuery = (connection, query, escape = []) => {
+        return new Promise((resolve, reject) => {
+            connection.query(query, escape, (err, result) => {
+                // check err
+                err ? reject({code : 500, msg : err.sqlMessage}) : resolve(result)
+            })
+        })
+    }
+
+    dbPoolQueryTransaction = (dbConnection, res, callback) => {
+        dbConnection.getConnection((err, connection) => {
+            if (err) {
+                connection.rollback()
+                connection.release()
+                return
+            }
+            connection.beginTransaction( async err => {
+                try {
+                    await callback(connection)
+                    await connection.commit()
+                } catch (err) {
+                    await connection.rollback()
+                    res.status(err.code||500).send(err.msg)
+                } finally {
+                    await connection.release()
                 }
             })
         })
     }
-    // create error handle for databaseQuery()
-    databaseQueryWithErrorHandle = async (res, callback) => {
-        try {
-            await callback()
-        } catch (err) {
-            res.status(500).send(err)
-        }
-    }
-    // create promisfy using begin transaction
-    databaseQueryTransaction = async (res, callback) => {
-        try {
-            await database.beginTransaction( async (err) => {
-                if (err) throw err
-                // error handling for our custom function
-                try {
-                    await callback()
-                } catch (err) {
-                    console.log(err)
-                    await database.rollback() 
-                    res.status(500).send(err) 
-                }
-                await database.commit()
-            })
-        } catch (err) {
-            await database.rollback() 
-        }
-    }
 }
 
-module.exports = {
-    con : database => new Connection (database)
+module.exports = { 
+    connection : db => new SingleConnection(db), 
+    poolConnection : _ => new PoolConnection()
 }
